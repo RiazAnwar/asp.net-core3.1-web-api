08459757ea8ffa13d78ef43ea13297723cfc5a70
RiazAnwar <iamriazanwar@gmail.com>
2020-10-08 00:36:31 +0500

    Api Project

---
+++ CoreApiTesting/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiTesting.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet("getname")]
+            public string GetName()
+        
+        {
+            return "Riaz Anwar";
+        }
+        [HttpGet("getmarks")]
+        public int GetMarks(int rollnumber)
+        {
+            return 100;
+        }
+        [HttpPost("saveuser")]
+        public string Save(StudentDTO dto)
+        {
+            return "Done";
+        }
+    }
+
+    public class StudentDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
